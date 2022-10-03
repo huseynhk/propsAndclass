f14c1dd5cb8436b89277f3a69a40f3073816fea6
huseyn <khuseyn693@gmail.com>
2022-10-03 22:53:31 +0400

    useState

--- src/components/collapse.js
+++ src/components/collapse.js
@@ -2,22 +2,66 @@ import React from 'react';
 
 
 
-const Collapse = (props) => {
-return (
-  <div>
+class Collapse extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      showContent: false
+    }
     
-  <a class="btn btn-warning" data-bs-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
-    Click
-  </a> 
- 
-<div class="collapse show" id={props.href}>
-  <div class="card card-body">
-    {props.children}
-  </div>
-</div>
-  </div>
-);
-};
+    // this.showMore = this.showMore.bind(this)
+
+  }
+
+  // showMore(){
+    // this.setState({showContent: true})
+    //  }
+
+   showMore = () => {
+     this.setState ({showContent: !this.state.showContent})
+   }
+
+
+  render() {
+    return (
+      //   <div>
+      //   <a class="btn btn-warning" data-bs-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+      //     Click
+      //   </a> 
+
+      // <div class="collapse show" id={this.props.href}>
+      //   <div class="card card-body">
+      //     {this.props.children}
+      //     {this.state.showContent}
+      //   </div>
+      // </div>
+      //   </div>
+
+
+      <div>
+        <button class="btn btn-warning" onClick={this.showMore}>
+          Click
+        </button>
+
+        {
+          this.state.showContent ? (
+
+            <div class="collapse show">
+              <div class="card card-body">
+                {this.props.children}
+              </div>
+            </div>
+
+
+          ) : null
+        }
+      </div>
+    );
+  };
+}
+
 
 export default Collapse
 
